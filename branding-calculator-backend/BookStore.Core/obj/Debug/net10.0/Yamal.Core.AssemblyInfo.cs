153f88e200de4b3b75bcf24afd116a9fcdb8520b
snaaas12 <kostya.sha.94@mail.ru>
2026-06-06 17:56:21 +0500

    nothing to commit

--- branding-calculator-backend/BookStore.Core/obj/Debug/net10.0/Yamal.Core.AssemblyInfo.cs
+++ branding-calculator-backend/BookStore.Core/obj/Debug/net10.0/Yamal.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yamal.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d69a8f2c5e7a0cb6ba8549de0f08ba486ba33eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d58dd39dca33a8c2875f17a8d7c3701ad7f3c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yamal.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yamal.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
